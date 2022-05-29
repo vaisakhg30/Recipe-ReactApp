d894d5e04d7a68f5edafbc19b8a961ccf2f1c5ad
vaisakhg0 <vaisakhg30@gmail.com>
2022-05-29 23:21:41 +0530

    recipe

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
 import Entry from './component/Entry';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Entry/>
+     <BrowserRouter>
+     <Routes> 
+<Route path='/' exact element={<Entry/>}/>
+
+     </Routes>
+     </BrowserRouter>
     </div>
   );
 }
